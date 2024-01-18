f01d5219c53ff2d4c59d3c1bfe23fc904caac54b
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 08:54:58 +0100

    add: scaffold

---
+++ TimeFrames.Api/Data/TimeFrameContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimeFrames.Api.Models;
+
+    public class TimeFrameContext : DbContext
+    {
+        public TimeFrameContext (DbContextOptions<TimeFrameContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TimeFrames.Api.Models.User> User { get; set; } = default!;
+    }
