7e5a91ccd9c54b4c0fdef024b7988377396c2a7e
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 23:50:35 +0100

    add: new migration

---
+++ TimeFrames.Api/Migrations/20240118224927_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeFrames.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaskType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompletionTime = table.Column<int>(type: "int", nullable: false),
+                    TypeColor = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
