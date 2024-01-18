8b2783293096f9ace0085091ec8e17a736190c5c
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 08:44:23 +0100

    update: models

---
+++ TimeFrames.Api/Models/DayData.cs
@@ -0,0 +1,11 @@
+namespace TimeFrames.Api.Models;
+
+public class DayData
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public double WorkTime { get; set; }
+    public double WakeTime { get; set; }
+    public double SleepTime { get; set; }
+    public Task[] Tasks { get; set; }
+}
--- TimeFrames.Api/Models/User.cs
+++ TimeFrames.Api/Models/User.cs
@@ -1,6 +1,11 @@
+using Microsoft.Build.Framework;
+
 namespace TimeFrames.Api.Models;
 
 public class User
 {
-    
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; }
+    public DayData[] Dates { get; set; }
 }
