398e7f6c976878472c4637519198245c17fba1dd
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 23:50:29 +0100

    add: property

--- TimeFrames.Api/Models/ToDo.cs
+++ TimeFrames.Api/Models/ToDo.cs
@@ -4,6 +4,7 @@ public class ToDo
 {
     public int Id { get; set; }
     public string Content { get; set; }
+    public string TaskType { get; set; }
     public int  CompletionTime { get; set; }
     public string TypeColor { get; set; }
 }
